baa3f3d0b395cdc3d6ac2dc363561d19fc56eb8c
Charles Luxinger <charlesluxinger@hotmail.com>
2019-07-27 10:25:46 -0300

    1. Initialize Repository

---
+++ workshopmongospring/src/main/java/com/example/workshopmongospring/WorkshopmongospringApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshopmongospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongospringApplication.class, args);
+	}
+
+}
---
+++ workshopmongospring/src/test/java/com/example/workshopmongospring/WorkshopmongospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshopmongospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
