a891fdc98ca980fbaac9146f3450b670709a6291
Charles Luxinger <charlesluxinger@hotmail.com>
2019-07-27 13:36:37 -0300

    6. Implements FindById

--- workshopmongospring/src/main/java/com/example/workshopmongospring/services/UserService.java
+++ workshopmongospring/src/main/java/com/example/workshopmongospring/services/UserService.java
@@ -1,12 +1,14 @@
 package com.example.workshopmongospring.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshopmongospring.domain.User;
 import com.example.workshopmongospring.repository.UserRepository;
+import com.example.workshopmongospring.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findByID(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado!!!"));
 	}
 }
