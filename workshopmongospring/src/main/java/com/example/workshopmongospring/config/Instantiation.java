609ca15f62f0aa325ca336582164b5143bd862b2
Charles Luxinger <charlesluxinger@hotmail.com>
2019-07-28 19:32:13 -0300

    14. Add comments at Post

--- workshopmongospring/src/main/java/com/example/workshopmongospring/config/Instantiation.java
+++ workshopmongospring/src/main/java/com/example/workshopmongospring/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.example.workshopmongospring.domain.Post;
 import com.example.workshopmongospring.domain.User;
 import com.example.workshopmongospring.dto.AuthorDTO;
+import com.example.workshopmongospring.dto.CommentDTO;
 import com.example.workshopmongospring.repository.PostRepository;
 import com.example.workshopmongospring.repository.UserRepository;
 
@@ -40,6 +41,14 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para SP... Good bye!",
 				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1  = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2  = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3  = new CommentDTO("Tenha uma boa viagem...", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComennts().addAll(Arrays.asList(c1,c2));
+		post2.getComennts().addAll(Arrays.asList(c3));
+		
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
--- workshopmongospring/src/main/java/com/example/workshopmongospring/domain/Post.java
+++ workshopmongospring/src/main/java/com/example/workshopmongospring/domain/Post.java
@@ -1,12 +1,15 @@
 package com.example.workshopmongospring.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.example.workshopmongospring.dto.AuthorDTO;
+import com.example.workshopmongospring.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String body;
 
 	private AuthorDTO autor;
+
+	private List<CommentDTO> comennts = new ArrayList<>();
 
 	public Post() {
 	}
@@ -70,6 +75,14 @@ public class Post implements Serializable {
 
 	public void setAutor(AuthorDTO autor) {
 		this.autor = autor;
+	}
+	
+	public List<CommentDTO> getComennts() {
+		return comennts;
+	}
+
+	public void setComennts(List<CommentDTO> comennts) {
+		this.comennts = comennts;
 	}
 
 	@Override
